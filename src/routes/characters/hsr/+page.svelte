c8e44f8bac0b91be2bf304e8037c041705bf87c4
loudclaw <greycloudsfall@gmail.com>
2023-08-23 19:29:41 -0400

    converted all png into webp

--- src/routes/characters/hsr/+page.svelte
+++ src/routes/characters/hsr/+page.svelte
@@ -3,6 +3,6 @@
     export let data
 </script>
 {#each data.characters as {name}}
-<li><a href="{base}/characters/hsr/{name}"><img src="{base}/img/{name}.png" alt=""></a>
+<li><a href="{base}/characters/hsr/{name}"><img src="{base}/img/{name}.webp" alt=""></a>
 </li>
 {/each}
